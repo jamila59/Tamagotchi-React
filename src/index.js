5baec25b854b299746d84e9b0700926551747129
Student <example@example.com>
2020-02-19 15:48:17 -0800

    init proj.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+var heading = React.createElement('h1', {}, 'Help Queue');
+var ticketLocation = React.createElement('h3', {}, '3a');
+var ticketNames = React.createElement('h3', {}, 'Thato and Haley');
+var ticketIssue = React.createElement('h3', {}, 'Firebase won\'t save record');
+var app = React.createElement('div', {}, heading, ticketLocation, ticketNames, ticketIssue);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  app,
+  document.getElementById('react-app-root')
+);
